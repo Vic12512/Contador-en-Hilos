87d9b72e6da5627b988323a0b06013acb2697a9f
Victor Cuevas <victoreliecer165@gmail.com>
2024-10-30 23:15:22 -0400

    Add project files.

---
+++ Contador en Hilos/Contador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contador_en_Hilos
+{
+    internal class Contador
+    {
+        private int _value;
+        private int _range;
+        private Thread _thread;
+        private bool _active;
+
+        public Contador(int range)
+        {
+            _value = 0;
+            _range = range;
+            _active = false;
+        }
+
+        public void start()
+        {
+            _active = true;
+            _thread = new Thread(execute);
+            _thread.Start();
+        }
+        public void stop()
+        {
+            _active = false;
+            _thread.Join();
+        }
+
+        public void execute()
+        {
+            while (_active)
+            {
+                _value++;
+                Console.WriteLine($"Contador en {_value}");
+                Thread.Sleep(_range);
+            }
+        }
+
+        public int GetValue()
+        {
+            return _value;
+        }
+    }
+}
---
+++ Contador en Hilos/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contador_en_Hilos;
+
+class Program
+{
+    private static List<Contador> _contadores = new List<Contador>();
+
+    static void Main(string[] args)
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu de contadores");
+            Console.WriteLine("1. Iniciar un nuevo contador");
+            Console.WriteLine("2. Detener un contador");
+            Console.WriteLine("3. Ver estado de los contadores");
+            Console.WriteLine("4. Salir");
+
+            Console.Write("Seleccione una opción: ");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    StartCounter();
+                    break;
+
+                case "2":
+                    StopCounter();
+                    break;
+
+                case "3":
+                    ShowStateCounter();
+                    break;
+
+                case "4":
+                    StopAllCunter();
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+        }
+    }
+
+    private static void StartCounter()
+    {
+        Console.Write("Ingrese el intervalo detiempo en miliseguntos");
+        int range = int.Parse(Console.ReadLine());
+
+        Contador Counter = new Contador(range);
+        Counter.start();
+        _contadores.Add(Counter);
+
+        Console.WriteLine("Contador Iniciado");
+    }
+
+    private static void StopCounter()
+    {
+        Console.Write("Ingrese el indice del contador a deterner");
+        int CounterIndex = int.Parse(Console.ReadLine());
+
+        if (CounterIndex >= 0 && CounterIndex < _contadores.Count)
+        {
+            _contadores[CounterIndex].stop();
+            Console.WriteLine("Contador detenido.");
+        }
+        else
+        {
+            Console.WriteLine("Índice no válido.");
+        }
+    }
+
+    private static void ShowStateCounter()
+    {
+        for (int i = 0; i < _contadores.Count; i++)
+        {
+            Console.WriteLine($"Contador {i}: {_contadores[i].GetValue()}");
+        }
+    }
+
+    private static void StopAllCunter() { 
+        foreach ( var contador in _contadores)
+        {
+            contador.stop();
+        }
+        Console.WriteLine("Todos los contadores se han detenido.");
+    }
+}
